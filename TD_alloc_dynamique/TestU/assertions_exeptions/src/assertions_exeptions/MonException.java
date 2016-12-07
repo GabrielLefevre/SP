79a59b0b982c9a63777c585c9ded57834189e281
gabriel.lefevre <gabriel.lefevre@info0427.iut-lens.univ-artois.fr>
2016-12-07 11:25:55 +0100

    WIP ex3 exc

---
+++ TD_alloc_dynamique/TestU/assertions_exeptions/src/assertions_exeptions/MonException.java
@@ -0,0 +1,11 @@
+package assertions_exeptions;
+
+public class MonException extends Exception {
+    MonException() {
+        super();
+    }
+
+    MonException(String s) {
+        super(s);
+    }
+}
